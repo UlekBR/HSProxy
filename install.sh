d62da14fce85f05a9dd4465c95f5279e4f2c6470
Paulo <127901575+UlekBR@users.noreply.github.com>
2025-04-11 13:15:36 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,21 +10,21 @@ arch=$(uname -m)
 
 if [[ $arch == "x86_64" || $arch == "amd64" || $arch == "x86_64h" ]]; then
     echo "Sistema baseado em x86_64 (64-bit Intel/AMD)"
-    curl -o "/opt/hsproxy/proxy" -f "https://github.com/UlekBR/HSProxy/blob/main/HSProxy_x64"
-    curl -o "/opt/hsproxy/sslproxy" -f "https://github.com/UlekBR/HSProxy/blob/main/HSProxySSL_x64"
-    curl -o "/opt/hsproxy/menu" -f "https://github.com/UlekBR/HSProxy/blob/main/menu_x64"
+    curl -o "/opt/hsproxy/proxy" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/HSProxy_x64"
+    curl -o "/opt/hsproxy/sslproxy" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/HSProxySSL_x64"
+    curl -o "/opt/hsproxy/menu" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/menu_x64"
 elif [[ $arch == "aarch64" || $arch == "arm64" || $arch == "armv8-a" ]]; then
     echo "Sistema baseado em arm64 (64-bit ARM)"
-    curl -o "/opt/hsproxy/proxy" -f "https://github.com/UlekBR/HSProxy/blob/main/HSProxy_arm64"
-    curl -o "/opt/hsproxy/sslproxy" -f "https://github.com/UlekBR/HSProxy/blob/main/HSProxySSL_arm64"
-    curl -o "/opt/hsproxy/menu" -f "https://github.com/UlekBR/HSProxy/blob/main/menu_arm64"
+    curl -o "/opt/hsproxy/proxy" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/HSProxy_arm64"
+    curl -o "/opt/hsproxy/sslproxy" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/HSProxySSL_arm64"
+    curl -o "/opt/hsproxy/menu" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/menu_arm64"
 else
     echo "Arquitetura não reconhecida: $arch"
     return
 fi
 
-curl -o "/opt/hsproxy/cert.pem" -f "https://github.com/UlekBR/HSProxy/blob/main/cert.pem"
-curl -o "/opt/hsproxy/key.pem" -f "https://github.com/UlekBR/HSProxy/blob/main/key.pem"
+curl -o "/opt/hsproxy/cert.pem" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/cert.pem"
+curl -o "/opt/hsproxy/key.pem" -f "https://raw.githubusercontent.com/UlekBR/HSProxy/refs/heads/main/key.pem"
 
 chmod +x /opt/hsproxy/proxy
 chmod +x /opt/hsproxy/sslproxy
